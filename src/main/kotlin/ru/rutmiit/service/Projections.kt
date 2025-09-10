95e5493a618899c5d1272c6d4f855e4de1efb8e9
Чеботарь Игорь Евгеньевич <chebotar@innovasystem.pro>
2025-09-11 00:34:25 +0300

    задание 3 сделал

--- src/main/kotlin/ru/rutmiit/service/Projections.kt
+++ src/main/kotlin/ru/rutmiit/service/Projections.kt
@@ -23,6 +23,9 @@ class Projections(
     private val logger = KotlinLogging.logger {}
 
     suspend fun getProductProjection(productId: UUID): Product? {
+        // 1. снимок бы хранил в эвент сторе в том же стриме, что и события. Иначе будет eventual consistency.
+        // 2. вместо fromStart() был бы offset снапшота, и события бы проигрывались поверх него.
+        // 3. после успешной записи порции событий.
         val product = repository.findById(productId) ?: return null
 
         val events = try {
